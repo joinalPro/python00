5348c8d37544032aabd0cbcfc2bf5286df4d8707
Joinal Abden <joinalzx@gmail.com>
2023-09-30 18:59:38 +0600

    variable recap

--- variable.py
+++ variable.py
@@ -1,4 +1,15 @@
+#This is variable
+
 name = "Joinal Abden"
 course = "Welcome to Python Master Class"
 
-print(name, course)
+#print(name, course)
+
+number1 = input("Please type an integer number and press enter: ")
+number2 = input("Please type another integer number and press enter: ")
+number1 = int(number1)
+number2 = int(number2)
+result = int( number1 + number2)
+
+print(result)
+
---
+++ welcome.py
@@ -0,0 +1,2 @@
+name = input("What is your Goal? ")
+print("Keep Focus,", name, "!")
