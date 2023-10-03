e0436baf2fe0a733311474fec0175ed5476cb952
Joinal Abden <joinalzx@gmail.com>
2023-10-03 19:10:36 +0600

    list item

---
+++ collections/collection.py
@@ -0,0 +1,9 @@
+st = ["tapazzul", "Rayhan Khan", "Abu", "Nasir", "Forhad"]
+#print(type(st))
+
+nums = ["Apple",23, 3,23, 11, 54.5]
+#print(nums, st)
+print(nums[0])
+
+name = "joinal abden"
+print(name[9])
