8a628b8302399f94db233ec0946e8e2cfe35fd9c
Joinal Abden <joinalzx@gmail.com>
2023-10-11 19:02:04 +0600

    list done

---
+++ collections/list_methods.py
@@ -0,0 +1,16 @@
+nums = [23, 45, 23, 1, 3, 43, 22, 77, 55, 47, 25, 97,11,11]
+
+nums[2] = 32
+print(nums)
+
+#print (dir(nums))
+#append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
+
+#print (nums.count(11))
+
+#nums2 = [34, 22, 56]
+
+#nums1.extend(nums2)
+#print(nums1)
+
+#print(nums1.index(11))
--- collections/list_slicing.py
+++ collections/list_slicing.py
@@ -1,2 +1,3 @@
 items = ["Apple",23, 3,23, 11, 54.5, "Banana", "orange", "yougurt"]
 
+print (items[3])
