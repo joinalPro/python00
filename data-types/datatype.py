ea2cba81f6ffb5a6fffceb355ac2ed79f55090bd
Joinal Abden <joinalzx@gmail.com>
2024-03-10 22:34:10 +0600

    data type

---
+++ data-types/datatype.py
@@ -0,0 +1,34 @@
+# name = "Joinal Abden"
+# age = 36
+
+# price = 22.50
+
+# student_list = ["nasir", "rayhan", "forhad","tapazzal", "tanvir"]
+# student_dict = { "name": "nasir", "age": 21, "country": "bd"}
+
+# is_student = True
+# is_student = False
+
+# set
+# tuple
+
+# price = 30
+# floatPrice = float(price)
+# print(floatPrice)
+
+# age = input("Please enter your age: ")
+# int_age = int(age)
+# print("Your age is:  ",int_age)
+
+# age = 20.5
+# str_age = str(age)
+# print(str_age)
+# print(type(str_age))
+
+# print(str(True))
+
+str_num = "10"
+print(type(str_num))
+int_num = int(str_num)
+print(int_num)
+print(type(int_num))
