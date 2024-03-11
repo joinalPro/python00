f464285cac4ada188fdcaa36f73c92f1e15ce192
Joinal Abden <joinalzx@gmail.com>
2024-03-11 09:10:45 +0600

    list slicing

--- collections/list_slicing.py
+++ collections/list_slicing.py
@@ -1,3 +1,13 @@
-items = ["Apple",23, 3,23, 11, 54.5, "Banana", "orange", "yougurt"]
+# items = ["Apple",23, 3,23, 11, 54.5, "Banana", "orange", "yougurt"]
+# print (items[3])
 
-print (items[3])
+# slicing
+nums = [1,2,3,4,5,6,7,8,9]
+# print(nums[2:5])
+# print(nums[:])
+# print(nums[3:])
+# print(nums[-5:-2])
+# print(nums[2:-2])
+
+# tell the step
+print(nums[2:-1:3])
